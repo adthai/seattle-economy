5da95d28343d9758b072ea2ae6e83a1e7a362c8c
adthai <adthai@uw.edu>
2024-07-05 22:24:22 -0700

    history skeleton

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -2,15 +2,27 @@ import * as Plot from "npm:@observablehq/plot";
 
 export function timeline(events, {width, height} = {}) {
   return Plot.plot({
-    width,
-    height,
-    marginTop: 30,
-    x: {nice: true, label: null, tickFormat: ""},
-    y: {axis: null},
-    marks: [
-      Plot.ruleX(events, {x: "year", y: "y", markerEnd: "dot", strokeWidth: 2.5}),
-      Plot.ruleY([0]),
-      Plot.text(events, {x: "year", y: "y", text: "name", lineAnchor: "bottom", dy: -10, lineWidth: 10, fontSize: 12})
-    ]
+marginLeft: 130,
+  axis: null,
+  x: {
+    axis: "top",
+    grid: true,
+    tickFormat: (x) => x < 0 ? `${-x} BC` : `${x} `
+  },
+  marks: [
+    Plot.barX(events, {
+      x1: "start",
+      x2: "end",
+      y: "event",
+      sort: {y: "x1"}
+    }),
+    Plot.text(events, {
+      x: "start",
+      y: "event",
+      text: "event",
+      textAnchor: "end",
+      dx: -3
+    })
+  ]
   });
 }
